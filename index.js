ac555d4a062ee32b233f501e7705be5ac97dad07
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-09-30 10:46:38 -0700

    bring in react

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 require('dotenv').config()
+const path = require('path')
 
 console.log(`web46 rulez!!!`)
-console.log(process.env.USER)
 
 const express = require('express')
 
 const server = express()
 server.use(express.json())
+server.use(express.static(
+    path.join(__dirname, 'client/build')
+))
 
-server.get('/', (req, res) => {
-    res.json({ message: 'web 46 rocks'})
+server.get('*', (req, res) => {
+    res.sendFile(
+        path.join(__dirname, 'client/build', 'index.html')
+    )
 })
 
 server.get('/hello', (req, res) => {
